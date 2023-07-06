65852bc5a7ad6f27ad36fdd2af3034f6cdcd93df
ByrdBassPC <byrdbass@gmail.com>
2023-07-06 13:39:11 -0500

    added some resources to modify style and images

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
+
+
+// //From morgan!
+//   document.addEventListener('DOMContentLoaded', function() {    
+//     var elems = document.querySelectorAll('.dropdown-trigger');    
+//     var instances = M.Dropdown.init(elems, "closeOnClick");  
+//   });
+// document.addEventListener('DOMContentLoaded', function() {    
+//   var elems = document.querySelectorAll('.sidenav');    
+//   var instances = M.Sidenav.init(elems, "closeOnClick");  
+// }); (edited) 
